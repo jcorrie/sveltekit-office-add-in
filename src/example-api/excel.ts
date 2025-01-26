767c85192cedf76b9d34a1518af6666ee54c6e3e
Jonny Corrie <jonnycorrie@gmail.com>
2025-01-26 13:34:10 +0000

    add excel interaction (no custom funct)

---
+++ src/example-api/excel.ts
@@ -0,0 +1,10 @@
+/* global Excel console */
+
+export async function writeToCell() {
+    await Excel.run(async function(context) {
+      let range = context.workbook.worksheets.getActiveWorksheet().getRange("A1")
+      range.values = [["Hello, World!"]];
+  
+      await context.sync();
+    });
+  }
--- src/routes/taskpane.html/+page.svelte
+++ src/routes/taskpane.html/+page.svelte
@@ -1,7 +1,9 @@
 <script>
 	import { addSlide } from '../../example-api/powerpoint';
+	import { writeToCell } from '../../example-api/excel';
 </script>
 
 <h1 class="bg-secondary-500">This is taskpane</h1>
 
 <button onclick={addSlide}> Add slide </button>
+<button onclick={writeToCell}> Write to cell </button>
